5059cb577f89f5d9c3388b8f69ccd477fb912a8d
Jocelyn FACCHINI <jocelyn.facchini@gmail.com>
2015-10-01 15:32:09 +0100

    Initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        throw new Exception('Not implemented');
+    }
+}
